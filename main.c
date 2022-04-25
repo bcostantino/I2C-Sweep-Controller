0c73101c70022617655b724f46daa365a8628fd7
bcostantino <costan96@students.rowan.edu>
2022-04-25 13:14:45 -0400

    cleanuo

--- main.c
+++ main.c
@@ -1,75 +1,3 @@
-/* --COPYRIGHT--,BSD_EX
- * Copyright (c) 2012, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *******************************************************************************
- * 
- *                       MSP430 CODE EXAMPLE DISCLAIMER
- *
- * MSP430 code examples are self-contained low-level programs that typically
- * demonstrate a single peripheral function or device feature in a highly
- * concise manner. For this the code may rely on the device's power-on default
- * register values and settings such as the clock configuration and care must
- * be taken when combining code from several examples to avoid potential side
- * effects. Also see www.ti.com/grace for a GUI- and www.ti.com/msp430ware
- * for an API functional library-approach to peripheral configuration.
- *
- * --/COPYRIGHT--*/
-//******************************************************************************
-//  MSP430G2xx3 Demo - USCI_B0 I2C Master RX single bytes from MSP430 Slave
-//
-//  Description: This demo connects two MSP430's via the I2C bus. The master
-//  reads from the slave. This is the master code. The data from the slave
-//  transmitter begins at 0 and increments with each transfer. The received
-//  data is in R5 and is checked for validity. If the received data is
-//  incorrect, the CPU is trapped and the P1.0 LED will stay on. The USCI_B0
-//  RX interrupt is used to know when new data has been received.
-//  ACLK = n/a, MCLK = SMCLK = BRCLK = default DCO = ~1.2MHz
-//
-//  *** to be used with "msp430g2xx3_uscib0_i2c_05.c" ***
-//
-//                                /|\  /|\
-//               MSP430G2xx3      10k  10k     MSP430G2xx3
-//                   slave         |    |        master
-//             -----------------   |    |  -----------------
-//           -|XIN  P1.7/UCB0SDA|<-|---+->|P1.7/UCB0SDA  XIN|-
-//            |                 |  |      |                 | 32kHz
-//           -|XOUT             |  |      |             XOUT|-
-//            |     P1.6/UCB0SCL|<-+----->|P1.6/UCB0SCL     |
-//            |                 |         |             P1.0|--> LED
-//
-//  D. Dang
-//  Texas Instruments Inc.
-//  February 2011
-//   Built with CCS Version 4.2.0 and IAR Embedded Workbench Version: 5.10
-//******************************************************************************
 #include <msp430.h>
 #include <stdio.h>
 
@@ -89,7 +17,6 @@ void initializeI2C()
     UCB0BR1 = 0;
     UCB0CTL1 &= ~UCSWRST;                     // Clear SW reset, resume operation
     IE2 |= UCB0RXIE + UCB0TXIE;
-    //UCB0I2CIE = UCNACKIE;
 }
 
 void initUART()
@@ -125,7 +52,6 @@ int main(void)
     {
         while (UCB0CTL1 & UCTXSTP);              // delay while stop condition is set
         UCB0CTL1 |= UCTXSTT;                     // set I2C start condition
-
         while (UCB0CTL1 & UCTXSTT);              // Start condition sent?
         UCB0CTL1 |= UCTXSTP;                     // I2C stop condition
 
@@ -180,6 +106,7 @@ void __attribute__ ((interrupt(USCIAB0RX_VECTOR))) USCI0RX_ISR (void)
     */
 }
 
+// RX/TX interrupt handler
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
 #pragma vector=USCIAB0TX_VECTOR
 __interrupt void USCI0TX_ISR(void)
