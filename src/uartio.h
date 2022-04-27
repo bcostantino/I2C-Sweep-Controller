8f038719475562ef1636f2f813739782338e4d89
bcostantino <costan96@students.rowan.edu>
2022-04-26 22:21:36 -0400

    more cleanup

--- src/uartio.h
+++ src/uartio.h
@@ -1,4 +1,3 @@
-void get_char_buffer_size(int* size, char* s);
-void uart_putsf(char* buffer, char* format);
+void get_char_buffer_size(unsigned int* size, char* s);
 void uart_puts(char* s);
 void uart_putc(char c);
